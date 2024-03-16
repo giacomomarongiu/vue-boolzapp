1e180ff67362bcb15e6ea2d2c1337067caad3ddb
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-03-16 10:32:47 +0100

    dubbugging -> Working on bug: when i search on bar and i write to an user there's an error

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            // Array di risposte random 
+            // Array di risposte random (importato)
             answers,
             // Variabile di appoggio per la data dei messaggi
             nowDate: "",
@@ -22,7 +22,7 @@ createApp({
             // Per le risposte automatiche creo una struttura dati di appoggio che sia la stessa dei messaggi già esistenti
             userReply: {
                 date: "now", //cambio dopo
-                message: "Ok",
+                message: "Ok", //cambio dopo
                 status: 'received', //sarà sempre received
             },
             // Creo una struttura dati di appoggio che sia la stessa dei messaggi già esistenti
@@ -52,7 +52,7 @@ createApp({
 
             if (this.searchedValue === "") {
                 this.printedContacts = this.contacts;
-                console.log(this.printedContacts.length > 0);
+                //console.log(this.printedContacts.length > 0);
             } else {
                 // Se NO li filtro
                 this.printedContacts = this.contacts.filter((searchString, index) => {
@@ -75,7 +75,7 @@ createApp({
             }
         },
 
-        //If condition for class received/sent
+        // Assegna la classe received/sent al messaggio
         sendOrReceived(string) {
             //faccio un leggo e vedo se prende giusto
             //console.log(string);
@@ -85,71 +85,113 @@ createApp({
             return 'received'
         },
 
-        //La funzione sendMessage mi permette di prendere il valore digitato in input e pusharlo/aggiungerlo ai messaggi già esistenti dell'utente attivo
+        // Permette di prendere il valore digitato in input e pusharlo/aggiungerlo ai messaggi già esistenti dell'utente attivo
         sendMessage() {
             //Verifico se è ben collegata
-            console.log("QUI IL MESSAGGIO UTENTE");
+            //console.log("QUI IL MESSAGGIO UTENTE");
             //Verifico cosa ho con This
-            console.log(this.contacts);
+            //console.log(this.contacts);
             //Conosco quale utente riceverà il messaggio perché ho userActive
-            console.log(this.contacts[this.userActive]);
+            //console.log(this.contacts[this.userActive]);
             // Verifico se è l'array giusto
-            console.log(this.contacts[this.userActive].messages);
+            //console.log(this.contacts[this.userActive].messages);
             // Verifico se newMessage ha il messaggio in lettura corretto
-            console.log(this.newMessage);
+            //console.log(this.newMessage);
             // Assegno a nowDate la data e l'ora di oggi
             this.dateMsg();
             // Assegno la data corretta al mio oggetto
             this.newMessage.date = this.nowDate;
+
+
+            const myName = this.printedContacts[this.userActive].name;
+            console.log(myName);
+            console.log(this.contacts);
+            (this.contacts).forEach((element, index) => {
+                console.log(element.name);
+                if (myName == element.name) {
+                    return this.userActive = index;
+                } else return console.log(false)
+            });
+
             // Aggiungo la todo all'array con l'operatore spred (push mi crea problemi per via del passaggio tramite reference)
+            console.log(this.userActive);
             (this.contacts[this.userActive].messages).push({ ...this.newMessage })
             // Pulisco l'input
+
             this.newMessage.message = "";
+
+            console.log("Qui");
+
             //Verifico se è stato aggiunto correttamente
-            console.log(this.contacts[this.userActive].messages);
-            setTimeout(() => { this.replyMessage() }, 1000) // Vedi sotto
+            //console.log(this.contacts[this.userActive].messages);
+            this.userBar()
+            setTimeout(() => { this.userBar(); this.replyMessage() }, 1000) // Vedi sotto
             // Se ho mando un messaggio a un utente questo diventa il primo della lista nella userBar
             //Utilizzo un var locale di appoggio
-            let myLastUser = this.contacts[this.userActive];
-            // Cancello l'oggetto appena creato
-            this.contacts.splice(this.userActive,1);
-            // Lo ri-inserisco, ma in cima
-            this.contacts.unshift(myLastUser);
-            // L'oggetto in cima avrà sempre indice 0
-            this.userActive=0;
+            setTimeout(() => {
+                let myLastUser = this.contacts[this.userActive];
+                // Cancello l'oggetto appena creato
+                this.contacts.splice(this.userActive, 1);
+                console.log("Qui");
+                // Lo ri-inserisco, ma in cima
+                this.contacts.unshift(myLastUser)
+                console.log("Qui");
+                // L'oggetto in cima avrà sempre indice 0
+                this.userActive = 0;
+                console.log("Qui");
+                
+                this.printedContacts.splice(this.userActive, 1);
+                console.log("Qui");
+                // Lo ri-inserisco, ma in cima
+                this.printedContacts.unshift(myLastUser)
+                console.log("Qui");
+                // L'oggetto in cima avrà sempre indice 0
+                this.userActive = 0;
+                console.log("Qui");
+                this.userBar()
+
+            }, 1000)
         },
 
-        //Creo una funzione che aggiunge un oggetto il cui testo è "ok" all'array dei messaggi nell'utente selezionato
+        // Aggiunge un oggetto il cui testo è una stringa random di "answers" all'array dei messaggi nell'utente attivo
         replyMessage() {
+            console.log("Qui");
+
             // Mi evito i log perché sono uguali a quelli di sendMessage
             // Unica differenza: il mio oggetto è statitico (userReply)
             // Assegno a nowDate la data e l'ora di oggi
             this.dateMsg();
             // Assegno la data corretta al mio oggetto
             this.userReply.date = this.nowDate;
+            console.log("Qui");
+
             // Genero una risposta random dal mio array di risposte 
             let randomIndex = Math.floor(Math.random() * (this.answers.length - 1));
             //console.log(this.answers[randomIndex]);
             // Assegno la risposta al mio oggetto
-            this.userReply.message=this.answers[randomIndex];
+            this.userReply.message = this.answers[randomIndex];
+            console.log("Qui");
+
             // Lo aggiungo
             (this.contacts[this.userActive].messages).push({ ...this.userReply })
+            console.log("Qui");
+
         },
 
         // Rimuove il messaggio relativo all'index passato
         removeMessage(userIndex) {
             //Verifico se si triggera bene
-            console.log("Cancella!");
+            //console.log("Cancella!");
             //Verifico l'index passato
-            console.log(userIndex);
+            //console.log(userIndex);
             //Verifico se è corretto l'elemento
-            console.log(this.contacts[this.userActive].messages);
-            console.log(this.contacts[this.userActive].messages[userIndex]);
+            //console.log(this.contacts[this.userActive].messages);
+            //console.log(this.contacts[this.userActive].messages[userIndex]);
             //Cancello il testo selezionato grazie all'indice che ho ricevuto
             (this.contacts[this.userActive].messages).splice(userIndex, 1)
         },
 
-        // Funzione che assegna la data/ora di oggi alla mia variabile nowDate 
+        // Assegna la data/ora di oggi alla mia variabile nowDate 
         dateMsg() {
             this.nowDate = DateTime.now()
                 .setLocale('it')
